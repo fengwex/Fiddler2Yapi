45d8e3f299e5d9a1f53c332a990c6b1109f76566
fengwex <fengwex@gmail.com>
2019-08-15 20:12:27 +0800

    finish fiddler to yapi

---
+++ bin/test.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+
+let res='{"errorCode":0,"errorStr":null,"results":{"salaryCount":3071,"successSalaryCount":3071,"salaryAmount":3071,"successSalaryAmount":307114d,"accountName":"网商银行","cardNumber":"6222202111102222","bankName":"网商银行","totalDrawAmount":0,"totalSubAmount":5142,"totalExpensesAmount":1070,"totalRechargeAmount":0,"balanceAmount":9999993783,"accountAmount":9999998929,"thirdPartyAmount":4,"thirdPartyShowStatus":true,"thirdPartyBalanceUpdateTime":null},"token":null}'
